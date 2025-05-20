c9568cab1c4cabc8bfb00325a451d4aba22e9cd5
RviP9973 <rp031776@gmail.com>
2025-05-20 09:39:10 +0530

    m

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "hhttp://todo-orpin-gamma.vercel.app",
+    origin: "http://todo-orpin-gamma.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
