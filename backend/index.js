67c60519126f6c75ef39f63b9f2e61162679693f
RviP9973 <rp031776@gmail.com>
2025-05-20 09:34:48 +0530

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "hhttp://todo-orpin-gamma.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
